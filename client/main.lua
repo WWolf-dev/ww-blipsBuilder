82dd41d6817e75381df970f3cdf6289f62beacba
Lycoris-Dev <140905559+WWolf-dev@users.noreply.github.com>
2023-09-21 16:03:04 +0200

    V1

--- client/main.lua
+++ client/main.lua
@@ -22,6 +22,13 @@ if FrameworkUse == "ESX" then
         PlayerLoaded = true
     end)
 
+    AddEventHandler("onResourceStart", function(resource)
+        if resource == GetCurrentResourceName() then
+            LoadAllBlipsFromDB()
+        end
+    end)
+
+
     -- Local function to display the Blips creation menu
     local function BlipsCreatorMenu()
         -- Menu structure
@@ -48,6 +55,7 @@ if FrameworkUse == "ESX" then
                         if DebugMode then
                             print("Running blip List")
                         end
+                        BlipsListMenu()
                     end
                 }
             }
@@ -117,6 +125,7 @@ if FrameworkUse == "ESX" then
             if DebugMode then
                 print("No input")
             end
+            lib.showContext("main_menu_blips_creator")
         end
     end
 
@@ -156,39 +165,228 @@ if FrameworkUse == "ESX" then
             color = tonumber(input[3]),
             alpha = tonumber(input[4]),
             coords = {
-                x = math.round(coords.x, 2),
-                y = math.floor(coords.y, 2),
-                z = math.floor(coords.z, 2)
+                x = coords.x,
+                y = coords.y,
+                z = coords.z,
             }
         }
 
         TriggerServerEvent('ww-blipsbuilder:Server:storeBlipInDB', blipData)
     end
 
-    function LoadAllBlipsFromDB()
-        -- Fetch all blips from the server
-        ESX.TriggerServerCallback('getAllBlipsFromDB', function(blips)
-            if blips then
-                for _, blipData in ipairs(blips) do
-                    -- Convert stored JSON coords back to a table
-                    local coords = json.decode(blipData.blip_coords)
+    -- Tableau pour stocker les blips avec leur ID comme clé
+local blipsOnMap = {}
 
-                    -- Create the blip using the retrieved data
-                    local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
+function LoadAllBlipsFromDB()
+    -- Fetch all blips from the server
+    ESX.TriggerServerCallback('getAllBlipsFromDB', function(blips)
+        if blips then
+            for _, blipData in ipairs(blips) do
+                -- Convert stored JSON coords back to a table
+                local coords = json.decode(blipData.blip_coords)
 
-                    SetBlipSprite(blip, blipData.blip_sprite)
-                    SetBlipDisplay(blip, 4)
-                    SetBlipScale(blip, blipData.blip_size)
-                    SetBlipColour(blip, blipData.blip_color)
-                    SetBlipAlpha(blip, blipData.blip_alpha)
-                    SetBlipAsShortRange(blip, true)
+                -- Create the blip using the retrieved data
+                local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
 
-                    -- Set blip name
-                    BeginTextCommandSetBlipName("STRING")
-                    AddTextComponentSubstringPlayerName(blipData.blip_name)
-                    EndTextCommandSetBlipName(blip)
-                end
+                SetBlipSprite(blip, blipData.blip_sprite)
+                SetBlipDisplay(blip, 4)
+                SetBlipScale(blip, blipData.blip_size)
+                SetBlipColour(blip, blipData.blip_color)
+                SetBlipAlpha(blip, blipData.blip_alpha)
+                SetBlipAsShortRange(blip, true)
+
+                -- Set blip name
+                BeginTextCommandSetBlipName("STRING")
+                AddTextComponentSubstringPlayerName(blipData.blip_name)
+                EndTextCommandSetBlipName(blip)
+
+                -- Stockez le blip dans le tableau en utilisant l'ID du blip comme clé
+                blipsOnMap[blipData.id] = blip
             end
+        end
+    end)
+end
+
+
+    function BlipsListMenu()
+        -- Demande au serveur la liste de tous les blips
+        ESX.TriggerServerCallback('getAllBlipsFromDB', function(blips)
+
+            -- Si aucune donnée n'est renvoyée, terminez la fonction ici
+            if not blips then return end
+
+            -- Construire le tableau des options pour le menu basé sur les blips reçus
+            local options = {}
+            for _, blipData in ipairs(blips) do
+                local coords = json.decode(blipData.blip_coords)
+
+                table.insert(options, {
+                    title = blipData.blip_name,
+                    description = string.format("Sprite: %s, Taille: %s, Couleur: %s, Opacité: %s",
+                        blipData.blip_sprite,
+                        blipData.blip_size,
+                        blipData.blip_color,
+                        blipData.blip_alpha
+                    ),
+                    onSelect = function()
+                        lib.registerContext({
+                            id = "blip_options_menu",
+                            title = "Options du Blip",
+                            menu = "blips_list_menu",
+                            options = {
+                                {
+                                    title = "Téléporter sur le blip",
+                                    description = "Téléporte le joueur au blip",
+                                    icon = "fa-solid fa-map-marker",
+                                    onSelect = function()
+                                        DoScreenFadeOut(2000)
+                                        Wait(2000)
+                                        ESX.Game.Teleport(PlayerPedId(), coords)
+                                        Wait(1000)
+                                        DoScreenFadeIn(2000)
+                                    end
+                                },
+                                {
+                                    title = "Modifier le blip",
+                                    description = "Modifier le blip avec toutes les options souhaitées",
+                                    icon = "fa-solid fa-map-marker",
+                                    onSelect = function()
+                                        local input = lib.inputDialog('Paramètres du blip', {
+                                            {
+                                                type = 'number',
+                                                label = 'Sprite du Blip',
+                                                default = blipData.blip_sprite,
+                                                icon = 'hashtag',
+                                                required = true,
+                                                min = 1,
+                                                max = 866
+                                            },
+                                            {
+                                                type = 'slider',
+                                                label = 'Taille du Blip',
+                                                default = blipData.blip_size,
+                                                icon = 'hashtag',
+                                                required = true,
+                                                min = 0.1,
+                                                max = 2.0,
+                                                step = 0.1
+                                            },
+                                            {
+                                                type = 'number',
+                                                label = 'Couleur du Blip',
+                                                default = blipData.blip_color,
+                                                icon = 'hashtag',
+                                                required = true,
+                                                min = 1,
+                                                max = 85
+                                            },
+                                            {
+                                                type = 'number',
+                                                label = 'Opacité du Blip',
+                                                default = blipData.blip_alpha,
+                                                icon = 'hashtag',
+                                                required = true,
+                                                min = 1,
+                                                max = 254
+                                            },
+                                            {
+                                                type = 'input',
+                                                label = 'Nom de votre Blip',
+                                                default = blipData.blip_name,
+                                                icon = 'hashtag',
+                                                required = true,
+                                                min = 2,
+                                                max = 20
+                                            }
+                                        })
+    
+                                        -- Si l'utilisateur fournit une entrée, mettez à jour le blip avec ces paramètres
+                                        if input then
+                                            
+                                            local hasChanged = false
+                                
+                                            -- Compare les nouvelles valeurs avec les anciennes pour détecter les changements
+                                            if tonumber(input[1]) ~= blipData.blip_sprite or
+                                               tonumber(input[2]) ~= blipData.blip_size or
+                                               tonumber(input[3]) ~= blipData.blip_color or
+                                               tonumber(input[4]) ~= blipData.blip_alpha or
+                                               input[5] ~= blipData.blip_name then
+                                                hasChanged = true
+                                            end
+                                
+                                            if hasChanged then
+                                                local updatedBlipData = {
+                                                    id = blipData.id,
+                                                    name = input[5],
+                                                    sprite = tonumber(input[1]),
+                                                    size = tonumber(input[2]),
+                                                    color = tonumber(input[3]),
+                                                    alpha = tonumber(input[4]),
+                                                    coords = {
+                                                        x = coords.x,
+                                                        y = coords.y,
+                                                        z = coords.z,
+                                                    }
+                                                }
+                                                TriggerServerEvent('ww-blipsbuilder:Server:updateBlipInDB', updatedBlipData)
+
+                                                local existingBlip = blipsOnMap[blipData.id]
+                                                if existingBlip then
+                                                    RemoveBlip(existingBlip)
+                                                end
+                                                
+                                                -- Now create a new blip with the updated data
+                                                local newBlip = AddBlipForCoord(coords.x, coords.y, coords.z)
+                                                SetBlipSprite(newBlip, tonumber(input[1]))
+                                                SetBlipDisplay(newBlip, 4)
+                                                SetBlipScale(newBlip, tonumber(input[2]))
+                                                SetBlipColour(newBlip, tonumber(input[3]))
+                                                SetBlipAlpha(newBlip, tonumber(input[4]))
+                                                SetBlipAsShortRange(newBlip, true)
+                                                
+                                                BeginTextCommandSetBlipName("STRING")
+                                                AddTextComponentSubstringPlayerName(input[5])
+                                                EndTextCommandSetBlipName(newBlip)
+                                                
+                                                -- Store the new blip in the table, replacing the old reference
+                                                blipsOnMap[blipData.id] = newBlip
+                                            else
+                                                print("Aucun changement n'a été fait!")
+                                            end
+                                        else
+                                            lib.showContext("blip_options_menu")
+                                            print("No input provided!")
+                                        end
+                                    end
+                                },
+                                {
+                                    title = "Supprimer le Blip",
+                                    description = "Supprime le blip de la carte",
+                                    icon = "fa-solid fa-trash",
+                                    onSelect = function()
+                                        local existingBlip = blipsOnMap[blipData.id]
+                                        if existingBlip then
+                                            RemoveBlip(existingBlip)
+                                        end
+                                        TriggerServerEvent('ww-blipsbuilder:Server:deleteBlipInDB', blipData.id)
+                                    end
+                                }
+                            }
+                        })
+                        lib.showContext("blip_options_menu")
+                    end
+                })
+            end
+
+            -- Enregistrement et affichage du menu
+            lib.registerContext({
+                id = "blips_list_menu",
+                title = "Liste des Blips",
+                menu = "main_menu_blips_creator",
+                options = options
+            })
+
+            lib.showContext("blips_list_menu")
         end)
     end
 
--- server/main.lua
+++ server/main.lua
@@ -62,6 +62,45 @@ if FrameworkUse == "ESX" then
         end)
     end)
 
+    RegisterServerEvent('ww-blipsbuilder:Server:updateBlipInDB')
+    AddEventHandler('ww-blipsbuilder:Server:updateBlipInDB', function(blipData)
+        local query = 'UPDATE blips SET blip_name = @name, blip_sprite = @sprite, blip_size = @size, blip_color = @color, blip_alpha = @alpha, blip_coords = @coords WHERE id = @id'
+        local values = {
+            ['@id'] = blipData.id,
+            ['@name'] = blipData.name,
+            ['@sprite'] = blipData.sprite,
+            ['@size'] = blipData.size,
+            ['@color'] = blipData.color,
+            ['@alpha'] = blipData.alpha,
+            ['@coords'] = json.encode(blipData.coords)
+        }
+
+        MySQL.Async.execute(query, values, function(rowsChanged)
+            if rowsChanged > 0 then
+                print("Blip updated!")
+            else
+                print("Error updating blip!")
+            end
+        end)
+    end)
+
+    RegisterServerEvent('ww-blipsbuilder:Server:deleteBlipInDB')
+    AddEventHandler('ww-blipsbuilder:Server:deleteBlipInDB', function(blipId)
+        local query = 'DELETE FROM blips WHERE id = @id'
+        local values = {
+            ['@id'] = blipId
+        }
+
+        MySQL.Async.execute(query, values, function(rowsChanged)
+            if rowsChanged > 0 then
+                print("Blip deleted!")
+            else
+                print("Error deleting blip!")
+            end
+        end)
+    end)
+
+
 
 
     
